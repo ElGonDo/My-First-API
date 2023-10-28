500bc0f3f7e150ac5b4dccf90b0c459c3e220d5c
Joseph López Henao <joseph.lopez2@soy.sena.edu.co>
2023-10-28 00:41:45 -0500

    API REST funcional, y usada con react

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,13 @@
 import productRouter from '#Routes/product.routes.js';
+import cors from 'cors';
 import express from 'express';
 
 const expressApp = express();
 
 // Middlewares
 expressApp.use(express.json());
+// Aplica el middleware de CORS
+expressApp.use(cors());
 
 // Routes
 expressApp.use('/productos', productRouter);
